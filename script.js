0d3f330cb16ba171343909806a9e56f9009fd5f3
Elxan <elxan.civishov2000@gmail.com>
2022-04-25 20:00:28 +0400

    finish

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+let arrayNumber = [],
+  evenResult = 1,
+  oddResult = 0,
+  number,
+  interval;
 
+function getValues() {
+  for (let i = 0; i < 4; i++) {
+    number = +prompt("Enter the number");
+    if (number % 2 == 0) {
+      evenResult *= number;
+    } else {
+      oddResult += number;
+    }
+  }
+  getResults();
+}
+
+getValues();
+
+function getResults() {
+  interval = evenResult - oddResult;
+  for (let i = interval; i >= 2; i--) {
+    if (i % 2 == 0) {
+      arrayNumber.push(i);
+    }
+  }
+}
+
+alert(`Odd Result : ${evenResult}
+Even Result : ${oddResult}
+interval : ${interval}
+Out Put : ${arrayNumber.join(",")}`);
